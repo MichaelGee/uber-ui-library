69322e487c077206a6e5c78cb66229bc506d8171
Juliet <julietadeboye01@gmail.com>
2023-10-11 14:35:20 +0100

    feat: created tooltip component

---
+++ src/components/tooltip/tooltip.stories.tsx
@@ -0,0 +1,27 @@
+import { StoryObj, Meta } from '@storybook/react';
+import Tooltip from './tooltip';
+
+const meta: Meta = {
+  title: 'Components/Tooltip',
+  component: Tooltip,
+  tags: ['autodocs'],
+  argTypes: {
+    side: {      
+        options: ['top', 'left', 'right', 'bottom']
+    },
+    variant: {
+      options: ['simple', 'icon']
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Tooltip>;
+
+export const Default: Story = (args) => <Tooltip {...args} />;
+Default.args = {
+  side: 'right',
+  variant: 'icon',
+  children: 'label',
+};
